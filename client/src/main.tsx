071257e8b93800fc9bea288eac490a85eefebe9d
Tyron Hakki Roan <hakkiroan@gmail.com>
2026-05-16 06:43:51 +0800

    feat: addtional pages on wrapper

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom/client"
 import {AuthProvider} from "./AuthContext";
 import App from "./App";
+import DashBoard from "./dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthProvider>
     <App />
+    <DashBoard />
   </AuthProvider>
 )
