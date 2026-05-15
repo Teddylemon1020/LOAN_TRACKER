2b52cbefc9787dce8923de28ca879b32fcf0d264
Tyron Hakki Roan <hakkiroan@gmail.com>
2026-05-16 07:57:49 +0800

    feat: unauthorized

---
+++ client/src/Unauthorized.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Unauthorized(){
+
+    return (
+        <div style= {{ textAlign: "center", marginTop: "100px"}}>
+            <h1> 403</h1>
+            <p> You do not have access to this page. </p>
+
+        </div>
+    )
+}
