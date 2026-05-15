eee22bdb269105b7dfea4b7bbc0338dae980361e
Tyron Hakki Roan <hakkiroan@gmail.com>
2026-05-16 06:42:13 +0800

    feat: login logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,122 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+interface Signup {
+  username: string;
+  password: string; 
+  email: string; 
+  confirmpassword: string;
+  role: string; 
+}
+
+interface Login {
+  username: Signup["username"];
+  password: Signup["password"]; 
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [signup, setSignup] = useState<Signup>({username: "", password: "", email: "", confirmpassword: "", role: "USER" });
+  const [login, setLogin] = useState<Login>({username: "", password: ""});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState (false);
+  const [isLoggedIn, setIsLoggedIn] = useState (false);
+  const [inSignup, setInSignup] = useState (false);
+  const [inLogIn, setInLogIn] = useState (false);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
+  const handSignup = async () =>{
+    setError("");
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+    if (!signup.username || !signup.password || !signup.confirmpassword || signup.email){
+      setError("Please fill all the fields");
+      return;
+    }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    setLoading(true);
+    try{
+
+      if (signup.password !== signup.confirmpassword) throw new Error("password and confirm password is not the same")
+
+      
+      const res = await fetch("/api/signup", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(signup),
+      });
+
+      
+      //email error and occupancy check 
+      if (!res.ok){
+        const errorData = await res.json();
+        throw new Error (errorData.email ?? errorData.message ?? "Email Taken or Error")
+      }
+
+      const data = await res.json();
+      localStorage.setItem("token", data.token);
+      setIsLoggedIn(true);
+    
+    } catch (err: any){
+      setError (err.message)
+    } finally{
+      setLoading(false);
+    }
+  }
+
+  const handleLogin = async() =>{
+    setError("")
+
+    try {
+      
+    } catch (err: any){
+      setError(err.message);
+    }finally{
+      setLoading(false);
+    }
+      
+    }
+  
+
+  
+
+  return (<div> 
+    {loading && <p> Loading...</p>}
+    {error && <p>{error}</p> }
+  </div>)
+  
 }
 
 export default App
